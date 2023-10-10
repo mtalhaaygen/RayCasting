ec5d1fcb300b9d966b9c51fac89c3597b0c2f42f
mtalhaaygen <mtalha.aygen18@gmail.com>
2023-10-10 19:39:20 +0300

    start parser

---
+++ libft/ft_strcmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maygen <maygen@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 16:18:00 by maygen            #+#    #+#             */
+/*   Updated: 2023/10/10 18:05:37 by maygen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_strcmp(const char *s1, const char *s2)
+{
+	if (!s1)
+		return (1);
+	while (*s1 == *s2++)
+	{
+		if (*s1++ == '\0')
+			return (0);
+	}
+	return (*(const unsigned char *)s1 - *(const unsigned char *)(s2 - 1));
+}
---
+++ libft/libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+int		ft_strcmp(const char *s1, const char *s2);
+
+#endif
