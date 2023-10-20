5c3782ff5c85b54bafe1309e1be7a833a7bbb0e8
mtalhaaygen <mtalha.aygen18@gmail.com>
2023-10-20 20:18:24 +0300

    x

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # RayCasting
+
+# Proje için
+
+.cub için olması gerekenler;
+
+- NO/SO/WE/EA/F/C değerlerinin sırası kendi araasında değişebilir, belirli bir sırayla gelmek zorunda değil. Ard arda gelmeli daha sonra harita gelecek
+- NO/SO/WE/EA/F/C değerleri her biri bir satırda belirtilmeli ve satırın başında sonunda NO ve ./path arasında boşluklar tablar olabilir
+- F/C değerleri 3er sayıdan oluşmalı ve bu sayılar yalnızca virgül ile ayrılabilir
+- NO/SO/WE/EA değerleri aynı olamaz
+- boş bir satırda sadece space tab varsa hata döndürülmeli
+- haritanın çevresi 1 ler ile çevrili olmalı, birbirine bağlı olmayan iki harita olamaz sadece bir harita olacak. Haritanın ulaşılabilir olmayan kısımları olabilir
--- main.c
+++ main.c
@@ -6,11 +6,36 @@
 /*   By: maygen <maygen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 08:22:35 by maygen            #+#    #+#             */
-/*   Updated: 2023/10/18 14:34:05 by maygen           ###   ########.fr       */
+/*   Updated: 2023/10/20 20:16:33 by maygen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	map_null(t_map	*map_value)
+{
+	map_value->c_color = NULL;
+	map_value->f_color = NULL;
+	map_value->no = NULL;
+	map_value->so = NULL;
+	map_value->we = NULL;
+	map_value->ea = NULL;
+}
+
+// char	*ft_trim(char	*s1)
+// {
+// 	int	i;
+// 	int	len;
+
+// 	i = 0;
+// 	len = ft_strlen(s1) - 1;
+// 	while (s1[i] && (s1[i] == 32 || s1[i] == 9)) // vertical tab => 9
+// 		i++;
+// 	while (s1[len] && (s1[len] == 32 || s1[len] == 9))
+// 		len--;
+//     s1[len + 1] = '\0';
+// 	return (s1 + i);
+// }
 
 void	map_reader2(t_map	*map_value, int fd, int i)
 {
@@ -21,71 +46,32 @@ void	map_reader2(t_map	*map_value, int fd, int i)
 	flag = 1;
 	j = 0;
 	while (i++ < map_value->map_height)
-	{
 		tmp = get_next_line(fd);
-		printf("tmp:%s", tmp);
-	}
-}
-
-int	color_read(t_map	*map_value, int fd, int i)
-{
-	int		flag;
-	char	*tmp;
-
-	flag = 1;
-	while (i++ < map_value->map_height)
-	{
-		tmp = get_next_line(fd);
-		if (flag == 1 && !ft_strncmp(tmp, "F", 1))
-		{
-			map_value->f_color = ft_strdup(tmp);
-			flag++;
-		}
-		else if (flag == 2 && !ft_strncmp(tmp, "C", 1))
-		{
-			map_value->c_color = ft_strdup(tmp);
-			flag++;
-		}
-		free(tmp);
-		if (flag > 2)
-			break ;
-	}
-	return (i);
 }
 
 int	floor_read(t_map	*map_value, int fd)
 {
 	char	*tmp;
 	int		i;
-	int		flag;
 
 	i = 0;
-	flag = 1;
 	while (i++ < map_value->map_height)
 	{
 		tmp = get_next_line(fd);
-		if (flag == 1 && !ft_strncmp(tmp, "NO", 2))
-		{
+		if (ft_strncmp(tmp, "NO", 2) == 0)
 			map_value->no = ft_strdup(tmp);
-			flag++;
-		}
-		else if (flag == 2 && !ft_strncmp(tmp, "SO", 2))
-		{
+		else if (ft_strncmp(tmp, "SO", 2) == 0)
 			map_value->so = ft_strdup(tmp);
-			flag++;
-		}
-		else if (flag == 3 && !ft_strncmp(tmp, "WE", 2))
-		{
+		else if (ft_strncmp(tmp, "WE", 2) == 0)
 			map_value->we = ft_strdup(tmp);
-			flag++;
-		}
-		else if (flag == 4 && !ft_strncmp(tmp, "EA", 2))
-		{
+		else if (ft_strncmp(tmp, "EA", 2) == 0)
 			map_value->ea = ft_strdup(tmp);
-			flag++;
-		}
+		else if (ft_strncmp(tmp, "F", 1) == 0)
+			map_value->f_color = ft_strdup(tmp);
+		else if (ft_strncmp(tmp, "C", 1) == 0)
+			map_value->c_color = ft_strdup(tmp);
 		free(tmp);
-		if (flag > 4)
+		if (map_value->no && map_value->so && map_value->we && map_value->ea && map_value->c_color && map_value->f_color)
 			break ;
 	}
 	return (i);
@@ -95,6 +81,7 @@ void	map_reader(t_map	*map_value)
 {
 	char	*tmp;
 	int		fd;
+	int		i;
 
 	tmp = ft_strdup("nane");
 	fd = open(map_value->map_name, O_RDONLY);
@@ -113,7 +100,8 @@ void	map_reader(t_map	*map_value)
 	fd = open(map_value->map_name, O_RDONLY);
 	if (fd == -1)
 		print_err("open error", NULL);
-	map_reader2(map_value, fd, color_read(map_value, fd, floor_read(map_value, fd)));
+	i = floor_read(map_value, fd);
+	map_reader2(map_value, fd, i);
 }
 
 int	print_err(char *str, char *arg)
@@ -135,11 +123,16 @@ int	filename_extension(char *str)
 	return (0);
 }
 
+ // tüm haritayı okuyup structdaki verileri dolduracak
+	// player bir tane olacak , W- E-N-S den bir tane olacak
+	// haritanın içinde space olabilir, ama tab olamaz, tüm spaceleri 1 yap
+	// wall_check(map_value->map); // map duvarlarını kontrol edecek
 t_map	*map_check(char **gv, t_map *map_value)
 {
 	map_value = malloc(sizeof(t_map));
 	filename_extension(gv[1]);
 	map_value->map_name = ft_strdup(gv[1]);
+	map_null(map_value);
 	map_reader(map_value);
 	return (map_value);
 }
@@ -163,7 +156,3 @@ int	main(int gc, char **gv)
 		return (print_err("invalid argument count", NULL));
 	// system("leaks cub3d");
 }
- // tüm haritayı okuyup structdaki verileri dolduracak
-	// player bir tane olacak , W- E-N-S den bir tane olacak
-	// haritanın içinde space olabilir, ama tab olamaz, tüm spaceleri 1 yap
-	// wall_check(map_value->map); // map duvarlarını kontrol edecek
